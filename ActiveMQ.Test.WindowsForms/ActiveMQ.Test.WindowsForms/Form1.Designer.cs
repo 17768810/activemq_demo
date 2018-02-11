4585926438d83a3b16c73aa981d232662eac9f5f
17768 <17768810@qq.com>
2018-02-11 17:10:53 +0800

    添加新项目。。。。

---
+++ ActiveMQ.Test.WindowsForms/ActiveMQ.Test.WindowsForms/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace ActiveMQ.Test.WindowsForms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox_queueName = new System.Windows.Forms.TextBox();
+            this.lbMessage = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 44);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(426, 242);
+            this.textBox1.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(22, 292);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox_queueName
+            // 
+            this.textBox_queueName.Location = new System.Drawing.Point(22, 13);
+            this.textBox_queueName.Name = "textBox_queueName";
+            this.textBox_queueName.Size = new System.Drawing.Size(216, 21);
+            this.textBox_queueName.TabIndex = 2;
+            this.textBox_queueName.Text = "wx_public_business9000155";
+            // 
+            // lbMessage
+            // 
+            this.lbMessage.Location = new System.Drawing.Point(22, 322);
+            this.lbMessage.Multiline = true;
+            this.lbMessage.Name = "lbMessage";
+            this.lbMessage.Size = new System.Drawing.Size(426, 109);
+            this.lbMessage.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(487, 443);
+            this.Controls.Add(this.lbMessage);
+            this.Controls.Add(this.textBox_queueName);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox_queueName;
+        private System.Windows.Forms.TextBox lbMessage;
+    }
+}
+
---
+++ ActiveMQ.Test.WindowsForms/ActiveMQ.Test.WindowsForms/Form1.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Apache.NMS;
+using Apache.NMS.ActiveMQ;
+
+namespace ActiveMQ.Test.WindowsForms
+{
+    public partial class Form1 : Form
+    {
+        private readonly IConnectionFactory factory;
+        public Form1()
+        {
+            InitializeComponent();
+
+            //failover:(tcp://172.18.20.132:61616)?timeout=5000&jms.useAsyncSend=true
+            //初始化工厂，这里默认的URL是不需要修改的
+            factory = new ConnectionFactory("tcp://172.18.20.132:61616");
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //通过工厂建立连接
+            using (IConnection connection = factory.CreateConnection())
+            {
+                //通过连接创建Session会话
+                using (ISession session = connection.CreateSession())
+                {
+                    //通过会话创建生产者，方法里面new出来的是MQ中的Queue
+                    IMessageProducer prod = session.CreateProducer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue(textBox_queueName.Text.Trim()));
+                    //创建一个发送的消息对象
+                    ITextMessage message = prod.CreateTextMessage();
+                    //给这个对象赋实际的消息
+                    message.Text = textBox1.Text;
+                    //设置消息对象的属性，这个很重要哦，是Queue的过滤条件，也是P2P消息的唯一指定属性
+                    //message.Properties.SetString("filter", "demo");
+                    //生产者把消息发送出去，几个枚举参数MsgDeliveryMode是否长链，MsgPriority消息优先级别，发送最小单位，当然还有其他重载
+                    prod.Send(message, MsgDeliveryMode.NonPersistent, MsgPriority.Normal, TimeSpan.MinValue);
+                    lbMessage.Text = "发送成功!!";
+                    //txtMessage.Text = "";
+                    //txtMessage.Focus();
+                }
+            }
+        }
+
+
+
+
+        //public void InitConsumer()
+        //{
+        //    //创建连接工厂
+        //    IConnectionFactory factory = new ConnectionFactory("tcp://localhost:61616");
+        //    //通过工厂构建连接
+        //    IConnection connection = factory.CreateConnection();
+        //    //这个是连接的客户端名称标识
+        //    connection.ClientId = "firstQueueListener";
+        //    //启动连接，监听的话要主动启动连接
+        //    connection.Start();
+        //    //通过连接创建一个会话
+        //    ISession session = connection.CreateSession();
+        //    //通过会话创建一个消费者，这里就是Queue这种会话类型的监听参数设置
+        //    IMessageConsumer consumer = session.CreateConsumer(new Apache.NMS.ActiveMQ.Commands.ActiveMQQueue("firstQueue"), "filter='demo'");
+        //    //注册监听事件
+        //    consumer.Listener += new MessageListener(consumer_Listener);
+        //    //connection.Stop();
+        //    //connection.Close();  
+
+        //}
+
+        //void consumer_Listener(IMessage message)
+        //{
+        //    ITextMessage msg = (ITextMessage)message;
+        //    //异步调用下，否则无法回归主线程
+        //    tbReceiveMessage.Invoke(new DelegateRevMessage(RevMessage), msg);
+
+        //}
+
+        //public delegate void DelegateRevMessage(ITextMessage message);
+
+        //public void RevMessage(ITextMessage message)
+        //{
+        //    tbReceiveMessage.Text += string.Format(@"接收到:{0}{1}", message.Text, Environment.NewLine);
+        //}
+    }
+}
